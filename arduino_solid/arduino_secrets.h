1e30d98aa47cba6ec8dd3cacce0c7079cfa69e27
frank smutniak <fsmutniak@gmail.com>
2021-09-22 16:10:38 -0500

    init arduino code submit, more coming

---
+++ arduino_solid/arduino_secrets.h
@@ -0,0 +1,5 @@
+#define WIFI_SSID "[your wifi SSID here]"
+#define WIFI_PASS "[your wifi password here]"
+#define SOLID_USERNAME "[your solid pod username here]"
+#define SOLID_PASSWORD "[your URL Encoded password here]" //URL ENCODED
+#define SOLID_URL "[your full pod URL here e.g. me.solidcommunity.net]"
